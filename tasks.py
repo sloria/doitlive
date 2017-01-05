4510df6cd1b25858ec4285516b8dab1b1a013f90
Steven Loria <sloria1@gmail.com>
2017-01-05 20:55:15 +0800

    Upgrade to invoke 0.14.0

--- tasks.py
+++ tasks.py
@@ -3,72 +3,72 @@ import os
 import sys
 import webbrowser
 
-from invoke import task, run
+from invoke import task
 
 docs_dir = 'docs'
 build_dir = os.path.join(docs_dir, '_build')
 
 @task
-def test(tox=False, lint=True, last_failing=False):
+def test(ctx, tox=False, lint=True, last_failing=False):
     """Run the tests.
 
     Note: --watch requires pytest-xdist to be installed.
     """
     if tox:
-        run('tox')
+        ctx.run('tox')
     else:
         import pytest
         if lint:
-            flake()
+            flake(ctx)
         args = []
         if last_failing:
             args.append('--lf')
         retcode = pytest.main(args)
         sys.exit(retcode)
 
-@task
-def flake():
+@task(aliases=['lint', 'flake8'])
+def flake(ctx):
     """Run flake8 on codebase."""
-    run('flake8 .', echo=True)
+    ctx.run('flake8 .', echo=True)
 
 @task
-def readme(browse=False):
-    run('rst2html.py README.rst > README.html')
+def readme(ctx, browse=False):
+    ctx.run('rst2html.py README.rst > README.html')
     if browse:
         webbrowser.open_new_tab('README.html')
 
 @task
-def clean():
-    run("rm -rf build")
-    run("rm -rf dist")
-    run("rm -rf doitlive.egg-info")
-    clean_docs()
+def clean(ctx):
+    ctx.run("rm -rf build")
+    ctx.run("rm -rf dist")
+    ctx.run("rm -rf doitlive.egg-info")
+    clean_docs(ctx)
     print("Cleaned up.")
 
 @task
-def publish(test=False):
+def publish(ctx, test=False):
     """Publish to the cheeseshop."""
-    clean()
+    clean(ctx)
     if test:
-        run('python setup.py register -r test sdist', echo=True)
-        run('twine upload dist/* -r test', echo=True)
+        ctx.run('python setup.py register -r test sdist', echo=True)
+        ctx.run('twine upload dist/* -r test', echo=True)
     else:
-        run('python setup.py register sdist', echo=True)
-        run('twine upload dist/*', echo=True)
+        ctx.run('python setup.py register sdist', echo=True)
+        ctx.run('twine upload dist/*', echo=True)
 
 
 @task
-def clean_docs():
-    run("rm -rf %s" % build_dir)
+def clean_docs(ctx):
+    ctx.run("rm -rf %s" % build_dir)
 
 @task
-def browse_docs():
-    run("open %s" % os.path.join(build_dir, 'index.html'))
+def browse_docs(ctx):
+    ctx.run("open %s" % os.path.join(build_dir, 'index.html'))
 
 @task
-def docs(clean=False, browse=False):
+def docs(ctx, clean=False, browse=False):
     if clean:
-        clean_docs()
-    run("sphinx-build %s %s" % (docs_dir, build_dir), pty=True)
+        clean_docs(ctx)
+    ctx.run("sphinx-build %s %s" % (docs_dir, build_dir), pty=True)
     if browse:
-        browse_docs()
+        browse_docs(ctx)
