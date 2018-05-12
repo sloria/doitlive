3c6b70f0e24d253d9dbd2048027906d70e293246
Steven Loria <sloria1@gmail.com>
2018-05-12 11:29:47 -0400

    Make version accessible from doitlive.__version__
    Also bump dev version

--- doitlive/__init__.py
+++ doitlive/__init__.py
@@ -12,9 +12,11 @@ from .styling import THEMES, Style, TermString, TTY, echo, echo_prompt, format_p
 from .keyboard import wait_for, magictype, magicrun, ESC, BACKSPACE, CTRLC, RETURNS
 from .version_control import get_current_git_branch, get_current_hg_branch, get_current_vcs_branch
 from .exceptions import DoItLiveError, ConfigurationError, SessionError
+from .__version__ import __version__
 
 __author__ = 'Steven Loria'
 __license__ = 'MIT'
+__version__ = __version__
 __all__ = [
     'SessionState',
     'PythonRecorderConsole',
