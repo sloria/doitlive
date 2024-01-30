049638ea96dc4c3c92aa1a24305961a71c35fbba
Steven Loria <sloria1@gmail.com>
2024-01-30 13:37:44 -0500

    Cleanup legacy ipython code; remove packaging and colorama as deps (#262)

--- doitlive/ipython/__init__.py
+++ doitlive/ipython/__init__.py
@@ -1,22 +1,8 @@
-import packaging.version
-
-
-def is_modern_ipython():
+def start_ipython_player(commands, speed):
     try:
-        import IPython
+        from doitlive.ipython.app import PlayerTerminalIPythonApp
     except ImportError:
         raise RuntimeError("ipython blocks require IPython to be installed")
-
-    return packaging.version.parse(IPython.__version__) >= packaging.version.parse(
-        "7.0.0"
-    )
-
-
-def start_ipython_player(commands, speed):
-    if is_modern_ipython():
-        from doitlive.ipython.app import PlayerTerminalIPythonApp
-    else:
-        from doitlive.ipython.legacy_app import PlayerTerminalIPythonApp
 
     PlayerTerminalIPythonApp.commands = commands
     PlayerTerminalIPythonApp.speed = speed
